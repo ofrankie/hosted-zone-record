4fe76518c7e2c1b1b3bd9f50d5bd4eb8309daabd
Frank Olufemi <ifrankie@Franks-MacBook-Air.local>
2019-04-08 14:43:58 +0100

    initial commit

---
+++ create-alias-record.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+  default = "eu-west-1"
+}
+
+data "aws_route53_zone" "selected" {
+  name         = "intexp.io."
+ # private_zone = true
+}
+
+module "terraform-aws-route53-alias" {
+  source          = "./modules/terraform-aws-route53-alias"
+  aliases         = ["dev.${data.aws_route53_zone.selected.name}"]
+  parent_zone_id  = "${data.aws_route53_zone.selected.zone_id}"
+  target_dns_name = "d1cmjnum02mk9e.cloudfront.net"
+  target_zone_id  = "Z2FDTNDATAQYW2"
+
+}  
+
